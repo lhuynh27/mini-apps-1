8e81f5c66159f8f991ce8631a9fb531d4caaee90
lhuynh27 <leannehuynh27@gmail.com>
2018-07-10 12:03:54 -0700

    Set up server using Expressjs

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const server = express();
 
+//serve static assets
+//create server
+server.use(express.static('client'))
+server.listen(3000, () => console.log('On port 3000 right meowww...'))
 
+//app-level middleware
+server.get('/HD', (req, res) => { 
+	res.send('HALLLLPPPP DESKKKK HAS ARRIVED')
+	//next()
+})
+
+server.post('/HD', (req, res) => {
+	res.send('CAAAALLLINGG HALLLLPPPP DESKKKK')
+	//next()
+})
