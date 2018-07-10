6e388b5f36d82cfb38e6653ccf607dab98ef5095
lhuynh27 <leannehuynh27@gmail.com>
2018-07-10 12:02:53 -0700

    Add get, post and button functionality

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,24 @@
+$(document).ready(function() {
 
+	$('button').click( () => { 
+		get();
+		console.log('I am hungry!'); 
+	});
 
+	$('form').on('submit', function(e){
+  		e.preventDefault(); //prevents auto-rerendering
+	});
+
+	get = () => {
+		$.get('http://127.0.0.1:3000/HD', () => {
+			console.log('GET: IMMA DUM DUM')
+		});
+	}
+
+	post = (input) => {
+		$.post('http://127.0.0.1:3000/HD', (input) => {
+			console.log('POST: IMMA DUM DUM')
+		});
+	}
+
+});
