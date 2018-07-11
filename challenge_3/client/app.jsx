e948153f42a98a1e597603f1c0e37f85a94093d1
lhuynh27 <leannehuynh27@gmail.com>
2018-07-11 11:14:05 -0700

    First commit

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Components {
 
 
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express')
+const server = express()
 
+server.listen(3000, () => console.log('3000 Ready To Go!'))
 
+server.get('/Main', function (req, res) {
+  res.send('GET Working')
+})
+
+server.post('/Main', function (req, res) {
+  res.send('POST Working')
+})
