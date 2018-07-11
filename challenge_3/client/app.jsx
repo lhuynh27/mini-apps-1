817ae99dea76abbf64bc54f70767911c7876232f
lhuynh27 <leannehuynh27@gmail.com>
2018-07-11 16:39:54 -0700

    Add f1, f2, f3 layouts

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,61 @@
-const App = () => (
-	<h2>Rendering Correctly</h2>
-	
-);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        page: ''
+      }
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1>Shopping Cart</h1>
+        <F1 />
+        <F2 />
+        <F3 />
+      </div>
+    )
+  }
+};
+
+const F1 = (props) => {
+      return(
+        <div>
+          <h3>Create Account</h3>
+          <input type="text" placeholder="Enter Name" size="30"/><br></br>
+          <input type="text" placeholder="Enter Email Address" size="30"/><br></br>
+          <input type="text" placeholder="Enter Password" size="30"/><br></br>
+          <button>Create Account</button>
+        </div>
+      ) 
+};
+
+const F2 = (props) => {
+      return(
+        <div>
+          <h3>Shipping Address</h3>
+          <input type="text" placeholder="Address Line 1" size="30"/><br></br>
+          <input type="text" placeholder="Address Line 1" size="30"/><br></br>
+          <input type="text" placeholder="City" size="30"/><br></br>
+          <input type="text" placeholder="State" size="14"/>
+          <input type="text" placeholder="Zip Code" size="14"/><br></br>
+          <input type="text" placeholder="Phone Number" /><br></br>
+          <button>Continue</button>
+        </div>
+      ) 
+};
+
+const F3 = (props) => {
+      return(
+        <div>
+          <h3>Payment Information</h3>
+          <input type="text" placeholder="Card Number" size="30"/><br></br>
+          <input type="text" placeholder="Expiration Date" size="14"/>
+          <input type="text" placeholder="CVV" size="14"/><br></br>
+          <input type="text" placeholder="Billing Zip Code" /><br></br>
+          <button>Continue</button>
+        </div>
+      ) 
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
