924465f5e4b0f1b34d8e4e50b6278d4b7c3c3d09
lhuynh27 <leannehuynh27@gmail.com>
2018-07-11 20:13:09 -0700

    Add page numbers

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,10 +2,9 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    //this.clickNext = this.clickNext.bind(this);
-      this.state = {
-        currentPage: ''
-      }
+    this.state = {
+      page: 0
+    }
   }
   
   handleChange(e){
@@ -15,24 +14,15 @@ class App extends React.Component {
     })
   }
 
-  // clickNext() => {
-  //   this.setState({
-  //     page: //page that will render next
-  //   })
-  // }
-  
+  clickNext(e){
+    this.setState({
+      page: e.target.page
+    })
+  }
+
   render(){
     return(
-      <div>
-        <h1>Shopping Cart</h1>
-        <FirstPage />
-        <br></br><br></br>
-        <F1 handleChange={this.handleChange} onClick={this.clickNext} /> 
-        <F2 handleChange={this.handleChange} onClick={this.clickNext} />
-        <F3 handleChange={this.handleChange} onClick={this.clickNext} />
-        <br></br><br></br>
-        <Confirmation />
-      </div>
+      <Confirmation />
     )
   }
 };
@@ -40,7 +30,7 @@ class App extends React.Component {
 const FirstPage = (props) => {
   return(
     <div>
-    <button>Checkout Now</button>
+    <button page="1" onClick={props.clickNext}>Checkout Now</button>
     </div>
   )
 };
@@ -52,7 +42,7 @@ const F1 = (props) => {
       <input type="text" placeholder="Name" size="30" name="name" onChange={props.handleChange} /><br></br>
       <input type="text" placeholder="Email Address" size="30" name="email" onChange={props.handleChange} /><br></br>
       <input type="text" placeholder="Password" size="30" name="password" onChange={props.handleChange} /><br></br> 
-      <button>Create Account</button>
+      <button page="2" onClick={props.clickNext}>Create Account</button>
     </div>
   )
 };
@@ -67,7 +57,7 @@ const F2 = (props) => {
       <input type="text" placeholder="State" size="14" name="state" onChange={props.handleChange} />
       <input type="text" placeholder="Zip Code" size="14" name="shipZip" onChange={props.handleChange} /><br></br>
       <input type="text" placeholder="Phone Number" name="phoneNumber" onChange={props.handleChange} /><br></br>
-      <button>Continue to Payment</button>
+      <button page="3" onClick={props.clickNext}>Continue to Payment</button>
     </div>
   )
 };
@@ -80,7 +70,7 @@ const F3 = (props) => {
       <input type="text" placeholder="Expiration Date" size="14" name="expDate" onChange={props.handleChange}/>
       <input type="text" placeholder="CVV" size="14" name="cvv" onChange={props.handleChange}/><br></br>
       <input type="text" placeholder="Billing Zip Code" name="billZip" onChange={props.handleChange}/><br></br>
-      <button>Continue to Confirmation</button>
+      <button page="4" onClick={props.clickNext}>Continue to Confirmation</button>
     </div>
   ) 
 };
@@ -88,7 +78,23 @@ const F3 = (props) => {
 const Confirmation = (props) => {
   return (
     <div>
-    <button>Purchase Now</button>
+      <h3>Personal Information</h3>
+        <p>{props.name}</p>
+        <p>{props.email}</p>
+        <p>{props.line1}</p>
+        <p>{props.line2}</p>
+        <p>{props.city}</p>
+        <p>{props.state}</p>
+        <p>{props.shipZip}</p>
+        <p>{props.phoneNumber}</p>
+    <br></br><br></br>
+      <h3>Credit Card Information</h3>
+        <p>{props.cardNumber}</p>
+        <p>{props.expDate}</p>
+        <p>{props.cvv}</p>
+        <p>{props.billZip}</p>
+    <br></br><br></br>
+    <button page="0" onClick={props.clickNext}>Purchase Now!</button>
     </div>
   )
 };
