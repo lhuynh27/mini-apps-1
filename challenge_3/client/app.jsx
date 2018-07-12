01fe4cc9dd5aeb7e938441d00d77a489f2d2a252
lhuynh27 <leannehuynh27@gmail.com>
2018-07-11 17:36:14 -0700

    Update html and input

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -5,12 +5,18 @@ class App extends React.Component {
         page: ''
       }
   }
+
+  // clickNext() => {
+  //   this.setState{
+  //     page: //page that the button was click on
+  //   }
+  // }
   
   render(){
     return(
       <div>
         <h1>Shopping Cart</h1>
-        <F1 />
+        <F1 /> 
         <F2 />
         <F3 />
       </div>
@@ -18,44 +24,123 @@ class App extends React.Component {
   }
 };
 
-const F1 = (props) => {
-      return(
-        <div>
-          <h3>Create Account</h3>
-          <input type="text" placeholder="Enter Name" size="30"/><br></br>
-          <input type="text" placeholder="Enter Email Address" size="30"/><br></br>
-          <input type="text" placeholder="Enter Password" size="30"/><br></br>
-          <button>Create Account</button>
-        </div>
-      ) 
+//IGNORE EVERYTHING IN BETWEEN <DIV>; testing to see how the forms look
+
+class F1 extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  // goes to the next form
+  // toAddressPage() {
+  //   e.preventDefault();
+  // }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+  
+  render(){
+    return(
+      <div>
+        <h3>Create Account</h3>
+        <input type="text" placeholder="Enter Name" size="30" onChange={this.handleNameChange.bind(this)} /><br></br>
+        <input type="text" placeholder="Enter Email Address" size="30" onChange={this.handleEmailChange.bind(this)} /><br></br>
+        <input type="text" placeholder="Enter Password" size="30" onChange={this.handlePasswordChange.bind(this)} /><br></br> 
+        <button onClick={this.toAddressPage}>Create Account</button>
+      </div>
+    ) 
+  }
 };
 
-const F2 = (props) => {
+class F2 extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  handleLine1Change(e) {
+    this.setState({
+      line1: e.target.value
+    })
+  }
+
+  handleLine2Change(e) {
+    this.setState({
+      line2: e.target.value
+    })
+  }
+
+  handleCityChange(e) {
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  handleStateChange(e) {
+    this.setState({
+      state: e.target.value
+    })
+  }
+
+  handleShipZipChange(e) {
+    this.setState({
+      shipZip: e.target.value
+    })
+  }
+
+  handlePhoneNumberChange(e) {
+    this.setState({
+      phoneNumber: e.target.value
+    })
+  }
+
+  render(){
       return(
         <div>
           <h3>Shipping Address</h3>
-          <input type="text" placeholder="Address Line 1" size="30"/><br></br>
-          <input type="text" placeholder="Address Line 1" size="30"/><br></br>
-          <input type="text" placeholder="City" size="30"/><br></br>
-          <input type="text" placeholder="State" size="14"/>
-          <input type="text" placeholder="Zip Code" size="14"/><br></br>
-          <input type="text" placeholder="Phone Number" /><br></br>
+          <input type="text" placeholder="Address Line 1" size="30" onChange={this.handleLine1Change.bind(this)} /><br></br>
+          <input type="text" placeholder="Address Line 1" size="30" onChange={this.handleLine2Change.bind(this)}/><br></br>
+          <input type="text" placeholder="City" size="30"onChange={this.handleCityChange.bind(this)} /><br></br>
+          <input type="text" placeholder="State" size="14"onChange={this.handleStateChange.bind(this)} />
+          <input type="text" placeholder="Zip Code" size="14"onChange={this.handleShipZipChange.bind(this)} /><br></br>
+          <input type="text" placeholder="Phone Number" onChange={this.handlePhoneNumberChange.bind(this)} /><br></br>
           <button>Continue</button>
         </div>
-      ) 
+      )
+  } 
 };
 
-const F3 = (props) => {
-      return(
-        <div>
-          <h3>Payment Information</h3>
-          <input type="text" placeholder="Card Number" size="30"/><br></br>
-          <input type="text" placeholder="Expiration Date" size="14"/>
-          <input type="text" placeholder="CVV" size="14"/><br></br>
-          <input type="text" placeholder="Billing Zip Code" /><br></br>
-          <button>Continue</button>
-        </div>
-      ) 
+class F3 extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>Payment Information</h3>
+        <input type="text" placeholder="Card Number" size="30"/><br></br>
+        <input type="text" placeholder="Expiration Date" size="14"/>
+        <input type="text" placeholder="CVV" size="14"/><br></br>
+        <input type="text" placeholder="Billing Zip Code" /><br></br>
+        <button>Continue</button>
+      </div>
+    ) 
+  }
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
