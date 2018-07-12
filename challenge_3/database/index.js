424cf042c1ee1037600e2cc382b1ba9e1e3410be
lhuynh27 <leannehuynh27@gmail.com>
2018-07-11 19:30:52 -0700

    Add foreign key

--- challenge_3/database/index.js
+++ challenge_3/database/index.js
@@ -1,9 +1,18 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
+  host: 'localhost:3000'
   user: 'student',
   password: 'student',
   database: 'checkout'
 });
+
+connection.query((err, data) => {
+  if (err){
+    console.log('Error Connecting with mySQL and Express')
+  } else {
+    console.log('Successful Connection with mySQL and Express');
+  }
+})
 
 connection.connect();
 module.exports = connection;
