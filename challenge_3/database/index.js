2c3774b715a65b1a45ece6a7cc605b0e86bb2254
lhuynh27 <leannehuynh27@gmail.com>
2018-07-12 09:55:38 -0700

    Changed user to be compatible

--- challenge_3/database/index.js
+++ challenge_3/database/index.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'student',
-  password: 'student',
+  host: 'http://127.0.0.1',
+  user: 'root',
+  password: '',
   database: 'checkout'
 });
 
-connection.query('Select * from f1', (err, data) => {
-  if (err){
-    console.log('Error Connecting with mySQL and Express', err)
-  } else {
-    console.log('Successful Connection with mySQL and Express');
-    console.log(data);
-  }
-})
+// connection.query('Select * from f1', (err, data) => {
+//   if (err){
+//     console.log('Error Connecting with mySQL and Express', err)
+//   } else {
+//     console.log('Successful Connection with mySQL and Express');
+//     console.log(data);
+//   }
+// })
 
 module.exports = connection;
